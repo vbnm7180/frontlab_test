1ae76b01bbf5ccd0b413d47d7f176d286694a88a
vbnm7180 <vbnm7180@mail.ru>
2020-11-15 00:41:39 +0400

    master

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@ window.onload = function() {
             console.log(users);
             for (user of users) {
                 var item = document.createElement("div");
-                item.innerHTML = "<div class=\"list__item\"><img class=\"item__image\" src=\"" + user.picture.medium + "\"><div class=\"item__name\">" + user.name.first + "</div></div>"
+                item.className = "list__block";
+                item.innerHTML = "<div class=\"list__item\"><img class=\"item__image\" src=\"" + user.picture.medium + "\"><div class=\"item__name\">" + user.name.title.toUpperCase() + " " + user.name.first.toUpperCase() + " " + user.name.last.toUpperCase() + "</div></div>";
+
+                item.innerHTML = item.innerHTML + "<div class=\"popup\"><div class=\"popup__header\"><img src=\"images/cancel.svg\" class=\"exit\"></div><div class=\"popup__content\"><img src=\"" + user.picture.large + "\" class=\"popup__image\"><div class=\"popup__info\"><div class=\"popup__street\">Street: " + user.location.street.toUpperCase() + "</div><br><div class=\"popup__city\">City: " + user.location.city.toUpperCase() + "</div><br><div class=\"popup__state\">State: " + user.location.state.toUpperCase() + "</div><br><div class=\"popup__mail\">Email: " + user.email + "</div><br><div class=\"popup__phone\">Phone: " + user.phone + "</div><br></div></div></div>";
+
                 document.querySelector(".list__container").appendChild(item);
 
             }
@@ -17,4 +21,55 @@ window.onload = function() {
 
     }
     oAJAX.send();
+
+
+
 }
+
+/*
+document.querySelector(".list__container").onclick = function(e) {
+    if (e.target.className == 'list__item') {
+        console.log(1);
+    }
+}
+*/
+
+document.querySelector(".list__container").addEventListener('click', function(e) {
+    if (e.target.closest(".list__item")) {
+        e.target.closest(".list__item").nextSibling.style.display = "block";
+    }
+});
+
+document.querySelector(".list__container").addEventListener('click', function(e) {
+    if (e.target.className == 'exit') {
+        e.target.closest(".popup").style.display = "none";
+    }
+});
+
+document.querySelector(".sort__btn").addEventListener('click', function() {
+    var list = document.querySelectorAll(".list__block");
+    //console.log(list);
+    list = Array.from(list);
+
+
+
+    if (document.querySelector(".sort__types").value == "1") {
+        list.sort(function(a, b) {
+            //console.log(a.firstChild.textContent);
+            if (a.firstChild.textContent > b.firstChild.textContent) return 1;
+            else return -1;
+        });
+    } else {
+        list.sort(function(a, b) {
+            //console.log(a.firstChild.textContent);
+            if (a.firstChild.textContent < b.firstChild.textContent) return 1;
+            else return -1;
+        });
+    }
+    console.log(list);
+    var list__container = document.querySelector(".list__container").innerHTML;
+    list__container = "";
+    for (item of list) {
+        document.querySelector(".list__container").appendChild(item);
+    }
+});
